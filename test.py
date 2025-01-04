b72fd3a790cffdfee8cc5046401681d3db46b49b
AreeshaFurqan <areesha2592004@gmail.com>
2025-01-04 20:30:52 +0500

    some upgrades

---
+++ test.py
@@ -0,0 +1,8 @@
+<div class="card_container">
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id voluptatibus animi quasquibusdam!Sint ipsum explicabo molestiae excepturi alias itaque porro impedit! Magni, nesciunt iusto. Optioquisquam ad!</p>
+            </div>
+            <div class="image_div">
+                <img src="../static/img/img1.jpg" class="card_img" alt="">
+            </div>
+        </div>
