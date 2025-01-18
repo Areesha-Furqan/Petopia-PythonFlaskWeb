c49fdc71e6e3b5b38853e0f9bcb6d3306aab4979
AreeshaFurqan <areesha2592004@gmail.com>
2025-01-18 13:23:31 +0500

    some changes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
-# Dummy users data
-users = {}
-user={}
+
+
+users = {} # variable initialization
+
+with open('users.json') as json_file:
+    users = json.load(json_file)
+print(users)
+
+
 
 @app.route('/home')
 def home():
@@ -22,6 +29,8 @@ def service():
 def gallery():
     return render_template('gallery.html')
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -37,6 +46,9 @@ def login():
     
     return render_template('login.html')  # Render login page on GET request
 
+
+
+
 @app.route('/reg', methods=['GET', 'POST'])
 def reg():
     if request.method == 'POST':
@@ -51,9 +63,17 @@ def reg():
         # Add user to the dictionary
         users[email] = password
         success_message = "Registration successful! Please log in."
+        print(" 54 users", users)
+        with open('users.json', 'w') as json_file:
+            json.dump(users, json_file, indent=4)            
+
         return redirect(url_for('login', success=success_message))  # Redirect to login page with success message
     
+    
     return render_template('reg.html')  # Render registration page
+
+
+
 
 @app.route('/contact')  # Contact page
 def contact():
